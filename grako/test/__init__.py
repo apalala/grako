9917421a7367913c376d0a0dc2aa8589c8702ec3
Juancarlo Añez <juancarlo.anez@gmail.com>
2013-10-08 16:24:51 -0430

    Run all tests as part of a single suite.

--- grako/test/__init__.py
+++ grako/test/__init__.py
@@ -5,10 +5,16 @@ The Grako test suite.
 from __future__ import print_function, division, absolute_import, unicode_literals
 
 if __name__ == '__main__':
+    import unittest
     from . import bootstrap_tests
-    from . import  buffering_test
-    from . import  grammar_tests
+    from . import buffering_test
+    from . import grammar_tests
 
     bootstrap_tests.main()
-    buffering_test.main()
-    grammar_tests.main()
+
+    suite = unittest.TestSuite(tests=[
+        buffering_test.suite(),
+        grammar_tests.suite()
+    ])
+
+    unittest.TextTestRunner(verbosity=2).run(suite)
