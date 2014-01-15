dcedb14260f37fe347034c71f8fe9d39728ef13a
Juancarlo Añez <juancarlo.anez@gmail.com>
2014-01-15 15:57:34 -0430

    Incorportated useful behavior from projects that use Grako into model nodes.

--- grako/model/base.py
+++ grako/model/base.py
@@ -6,22 +6,9 @@ Base definitions for models of programs.
 """
 from __future__ import print_function, division, absolute_import, unicode_literals
 from ..rendering import Renderer
+from ..ast import AST
 
 EOLCOL = 50
-
-
-class Context(object):
-    """ Context for source code generation.
-
-        It contains a copy of the source code when available,
-        a symbol table, and a stack of active views for symbols
-        that are used anonymously.
-    """
-
-    def __init__(self, **kwargs):
-        super(Context, self).__init__()
-        self._symbols = dict()
-        self.__dict__.update(kwargs)
 
 
 class Node(Renderer):
@@ -34,12 +21,39 @@ class Node(Renderer):
     """
 
     inline = True
-    template = ''
+    template = '{clasname}'
 
-    def __init__(self, ctx, parseinfo=None):
+    def __init__(self, ctx, ast=None, parseinfo=None):
         super(Node, self).__init__()
         self._ctx = ctx
+        if not parseinfo:
+            parseinfo = ast.parseinfo if isinstance(ast, AST) else None
         self._parseinfo = parseinfo
+
+        self.clasname = self.__class__.__name__
+        self._parent = None
+        self._children = []
+
+        if ast:
+            self._adopt_children(ast)
+            self.__postinit__(ast)
+
+    def __postinit__(self, ast):
+        pass
+
+    @property
+    def context(self):
+        return self.ctx
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @property
+    def line(self):
+        info = self.line_info
+        if info:
+            return info.line
 
     @property
     def ctx(self):
@@ -60,5 +74,18 @@ class Node(Renderer):
             text = self.parseinfo.buffer.text
             return text[self.parseinfo.pos:self.parseinfo.endpos]
 
+    def _adopt_children(self, ast):
+        if isinstance(ast, Node):
+            ast._parent = self
+            self._children.append(ast)
+        elif isinstance(ast, AST):
+            self._adopt_children(ast.values())
+        elif isinstance(ast, list):
+            for c in ast:
+                self._adopt_children(c)
+
     def __str__(self):
         return self.render()
+
+    def __repr__(self):
+        return str(self)
