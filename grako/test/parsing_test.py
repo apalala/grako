24859227da24e5c4e9f89e4c22d2d1d2337dc249
Juancarlo Añez <juancarlo.anez@gmail.com>
2014-05-22 22:21:21 -0430

    Added unit test for ..include::
    --HG--
    branch : includes

---
+++ grako/test/parsing_test.py
@@ -0,0 +1,38 @@
+
+# -*- coding: utf-8 -*-
+"""
+Grako language parsing tests.
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import unittest
+from ..util import trim
+from ..parser import GrakoBuffer
+
+
+class MockIncludeBuffer(GrakoBuffer):
+    def get_include(self, name):
+        return '\nINCLUDED "%s"\n' % name
+
+
+class ParsingTests(unittest.TestCase):
+    def test_include(self):
+        text = '''\
+            first
+            ..include :: something
+            last\
+        '''
+        buf = MockIncludeBuffer(trim(text))
+        self.assertEqual('first\n\nINCLUDED "something"\nlast', buf.text)
+
+
+def suite():
+    return unittest.TestLoader().loadTestsFromTestCase(ParsingTests)
+
+
+def main():
+    unittest.TextTestRunner(verbosity=2).run(suite())
+
+if __name__ == '__main__':
+    main()
