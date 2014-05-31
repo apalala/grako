f89219d7791467f10e3ad06e20034f6ca3e6ecca
Juancarlo Añez <juancarlo.anez@gmail.com>
2014-05-31 17:44:16 -0430

    Unused import.

--- grako/__init__.py
+++ grako/__init__.py
@@ -5,9 +5,7 @@ the described language.
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-
 from . import tool
-from .exceptions import FailedParse
 
 genmodel = tool.genmodel
 gencode = tool.gencode
