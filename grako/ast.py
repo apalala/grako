03924992b1c197ab697bd8c622c0505ace4a0876
Juancarlo Añez <juancarlo.anez@gmail.com>
2014-06-03 09:28:41 -0430

    Simplified AST.__iter()__ to the bare minimum.

--- grako/ast.py
+++ grako/ast.py
@@ -47,14 +47,7 @@ class AST(dict):
         upairs(kwargs.items())
 
     def __iter__(self):
-        assert set(self._order) == set(super(AST, self).__iter__())
-        order = set(self._order)
-        for k in self._order:
-            if k in self:
-                yield k
-        for k in super(AST, self).__iter__():
-            if not k in order:
-                yield k
+        return iter(self._order)
 
     def __setitem__(self, key, value):
         self._add(key, value)
@@ -104,7 +97,6 @@ class AST(dict):
             (k, v[:] if isinstance(v, list) else v)
             for k, v in self.items()
         )
-
 
     def _add(self, key, value, force_list=False):
         if self.__hasattribute__(key):
