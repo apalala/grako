fc07887d68981f302470d7231132d9d2c776d616
Juancarlo Añez <juancarlo.anez@gmail.com>
2014-06-08 10:46:27 -0430

    Don't use symbolic links.
    --HG--
    branch : 2.4

--- grako/test/__init__.py
+++ grako/test/__init__.py
@@ -4,13 +4,14 @@ The Grako test suite.
 """
 from __future__ import print_function, division, absolute_import, unicode_literals
 
-if __name__ == '__main__':
-    import unittest
-    from . import bootstrap_test
-    from . import buffering_test
-    from . import grammar_test
-    from . import codegen_test
+import unittest
+from . import bootstrap_test
+from . import buffering_test
+from . import grammar_test
+from . import codegen_test
 
+
+def main():
     suite = unittest.TestSuite(tests=[
         bootstrap_test.suite(),
         buffering_test.suite(),
@@ -19,3 +20,7 @@ if __name__ == '__main__':
     ])
 
     unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == '__main__':
+    main()
--- grako/test/__main__.py
+++ grako/test/__main__.py
@@ -1,1 +1,8 @@
-__init__.py
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+from grako import test
+
+
+if __name__ == '__main__':
+    test.main()
