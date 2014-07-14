2248ba156e7e3db0929a77087c593f89388e7bb9
Juancarlo Añez <juancarlo.anez@gmail.com>
2014-07-14 14:17:33 -0430

    Upgrade the regex example.

--- examples/regex/test.py
+++ examples/regex/test.py
@@ -5,7 +5,7 @@ from __future__ import (absolute_import, division, print_function,
 import sys
 
 import grako
-from grako.codegen import python
+from grako.codegen import codegen
 import regex_parser
 
 PARSER_FILENAME = 'genparser.py'
@@ -24,7 +24,7 @@ def main():
     print(grammar)
     sys.stdout.flush()
     with open(PARSER_FILENAME, 'w') as f:
-        f.write(python.codegen(model))
+        f.write(codegen(model))
     print('Generated parser saved as:', PARSER_FILENAME, file=sys.stderr)
     print(file=sys.stderr)
 
