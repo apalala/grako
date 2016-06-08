349dc235028fb76f1f445fb00b1687b315a404e2
Robert Siemer <Robert.Siemer-mercurial@backsla.sh>
2016-06-09 00:03:34 +0200

    improve command line help message and argument handling
    - tool.py
      - move argument parsing into a function
      - use argparse functionality for error-exit and groups
      - group “main mode” options to mutually exclusive group
        - create --generate-parser option (default) to underline what grako
          does by default
        - there is no easy way to specify a title to a mutually exclusive group
      - group options which influence the ebnf reading/parsing
      - group options which change the generated parser
      - group ordinary options together and put them last
        - requires manual adding --help option
      - existing help message used non-existant --outfile option
        - I found myself mixing them up
        - thus adding --outfile as --output alternative
        - code uses now only one name internally (outfile)
      - avoid model → result calculation in case of --draw
      - remove unused outer_version
      - remove args repacking; use args.thing directly
      - remove outfile unlinking
        - both pygraphviz and code generation do properly overwrite it themselves
      - remove superfluous default=True/False for store_false/store_true action
      - avoid --no-nameguard postprocessing with improved default
      - swap short with long option to get more
        expressive --help summary (see below)
    - simplify __init__.py
      - keep functinality the same
      - add “noqa” to unused imports in __init__.py
        - I believe the intention was to have gencode, genmodel and __version__
          available flat under grako.gencode, grako.genmode and grako.__version__
    
    usage: grako [--generate-parser | --draw | --object-model | --pretty]
                 [--color] [--trace] [--no-left-recursion] [--name NAME]
                 [--no-nameguard] [--outfile FILE] [--whitespace CHARACTERS]
                 [--help] [--version]
                 GRAMMAR
    
    --HG--
    branch : siemer_argparse

--- grako/__init__.py
+++ grako/__init__.py
@@ -1,20 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import functools
+from grako._version import __version__  # noqa
+from grako.tool import gencode, genmodel, main  # noqa
 
-from grako import tool
-from grako import _version
-from grako.codegen import pythoncg
-
-__version__ = _version.__version__
-
-genmodel = tool.genmodel
-gencode = functools.partial(tool.gencode, codegen=pythoncg)
-
-
-def main():
-    tool.main(codegen=pythoncg, outer_version='')
 
 if __name__ == '__main__':
     main()
--- grako/tool.py
+++ grako/tool.py
@@ -25,66 +25,91 @@ DESCRIPTION = (
 )
 
 
-argparser = argparse.ArgumentParser(prog='grako',
-                                    description=DESCRIPTION
-                                    )
-argparser.add_argument('-c', '--color',
-                       help='use color in traces (requires the colorama library)',
-                       action='store_true'
-                       )
-argparser.add_argument('-d', '--draw',
-                       help='generate a diagram of the grammar (requires --output)',
-                       action='store_true'
-                       )
-argparser.add_argument('filename',
-                       metavar='GRAMMAR',
-                       help='The filename of the Grako grammar'
-                       )
-argparser.add_argument('-g', '--object-model',
-                       help='generate object model from the class names given as rule arguments',
-                       dest="object_model",
-                       action='store_true',
-                       default=False
-                       )
-argparser.add_argument('-l', '--no-left-recursion',
-                       help='turns left-recusion support off',
-                       dest="left_recursion",
-                       action='store_false',
-                       default=True
-                       )
-argparser.add_argument('-m', '--name',
-                       nargs=1,
-                       metavar='NAME',
-                       help='Name for the grammar (defaults to GRAMMAR base name)'
-                       )
-argparser.add_argument('-n', '--no-nameguard',
-                       help='allow tokens that are prefixes of others',
-                       dest="nameguard",
-                       action='store_false',
-                       default=True
-                       )
-argparser.add_argument('-o', '--output',
-                       metavar='FILE',
-                       help='output file (default is stdout)'
-                       )
-argparser.add_argument('-p', '--pretty',
-                       help='prettify the input grammar',
-                       action='store_true'
-                       )
-argparser.add_argument('-t', '--trace',
-                       help='produce verbose parsing output',
-                       action='store_true'
-                       )
-argparser.add_argument('-w', '--whitespace',
-                       metavar='CHARACTERS',
-                       help='characters to skip during parsing (use "" to disable)',
-                       default=None
-                       )
+def parse_args():
+    argparser = argparse.ArgumentParser(prog='grako',
+                                        description=DESCRIPTION,
+                                        add_help=False)
+
+    main_mode = argparser.add_mutually_exclusive_group()
+    main_mode.add_argument('--generate-parser',
+                           help='generate parser code from the grammar (default)',
+                           action='store_true'
+                           )
+    main_mode.add_argument('--draw', '-d',
+                           help='generate a diagram of the grammar (requires --outfile)',
+                           action='store_true'
+                           )
+    main_mode.add_argument('--object-model', '-g',
+                           help='generate object model from the class names given as rule arguments',
+                           action='store_true'
+                           )
+    main_mode.add_argument('--pretty', '-p',
+                           help='generate a prettified version of the input grammar',
+                           action='store_true'
+                           )
+
+    ebnf_opts = argparser.add_argument_group('parse-time options',
+                                             'options in effect during ebnf grammar reading')
+    argparser.add_argument('filename',
+                           metavar='GRAMMAR',
+                           help='the filename of the Grako grammar to parse'
+                           )
+    ebnf_opts.add_argument('--color', '-c',
+                           help='use color in traces (requires the colorama library)',
+                           action='store_true'
+                           )
+    ebnf_opts.add_argument('--trace', '-t',
+                           help='produce verbose parsing output',
+                           action='store_true'
+                           )
+
+    generation_opts = argparser.add_argument_group('generation options',
+                                                   'options influencing the generated parser')
+    generation_opts.add_argument('--no-left-recursion', '-l',
+                                 help='turns left-recusion support off',
+                                 dest="left_recursion",
+                                 action='store_false'
+                                 )
+    generation_opts.add_argument('--name', '-m',
+                                 nargs=1,
+                                 metavar='NAME',
+                                 help='Name for the grammar (defaults to GRAMMAR base name)'
+                                 )
+    generation_opts.add_argument('--no-nameguard', '-n',
+                                 help='allow tokens that are prefixes of others',
+                                 dest="nameguard",
+                                 action='store_false',
+                                 default=None  # None allows grammar specified
+                                 )
+    generation_opts.add_argument('--outfile', '--output', '-o',
+                                 metavar='FILE',
+                                 help='output file (default is stdout)'
+                                 )
+    generation_opts.add_argument('--whitespace', '-w',
+                                 metavar='CHARACTERS',
+                                 help='characters to skip during parsing (use "" to disable)',
+                                 )
+
+    std_args = argparser.add_argument_group('ordinary options')
+    std_args.add_argument('--help', '-h',
+                          help='show this help message and exit',
+                          action='help')
+    std_args.add_argument('--version', '-v',
+                          help='provide version information and exit',
+                          action='version',
+                          version=__version__
+                          )
+
+    args = argparser.parse_args()
+
+    if args.draw and not args.outfile:
+        argparser.error('--draw requires --outfile')
+
+    return args
 
 
-def genmodel(name=None, grammar=None, trace=False, filename=None, colorize=False, **kwargs):
-    parser = GrakoGrammarGenerator(name, filename=filename, trace=trace, colorize=colorize, **kwargs)
-    return parser.parse(grammar, filename=filename, colorize=colorize, **kwargs)
+def genmodel(name=None, grammar=None, **kwargs):
+    return GrakoGrammarGenerator(name, **kwargs).parse(grammar, **kwargs)
 
 
 def gencode(name=None, grammar=None, trace=False, filename=None, codegen=pythoncg):
@@ -92,93 +117,55 @@ def gencode(name=None, grammar=None, trace=False, filename=None, codegen=pythonc
     return codegen(model)
 
 
-def _error(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+def main(codegen=pythoncg):
+    args = parse_args()
 
+    if args.whitespace:
+        args.whitespace = eval_escapes(args.whitespace)
 
-def main(codegen=pythoncg, outer_version=''):
-    argparser.add_argument(
-        '-v', '--version',
-        help='provide version information and exit',
-        action='version',
-        version=outer_version + __version__
-    )
-    try:
-        args = argparser.parse_args()
-    except Exception as e:
-        _error(str(e))
-        sys.exit(2)
-
-    colorize = args.color
-    filename = args.filename
-    name = args.name
-    nameguard = args.nameguard
-    draw = args.draw
-    outfile = args.output
-    pretty = args.pretty
-    object_model = args.object_model
-    trace = args.trace
-    whitespace = args.whitespace
-    left_recursion = args.left_recursion
-
-    if whitespace:
-        whitespace = eval_escapes(args.whitespace)
-
-    if draw and not outfile:
-        _error('--draw requires --outfile')
-        sys.exit(2)
-
-    if sum([draw, pretty, object_model]) > 1:
-        _error('only one of --draw, --pretty, --object_model allowed')
-        sys.exit(2)
-
-    # if name is None:
-    #    name = os.path.splitext(os.path.basename(filename))[0]
-
-    if outfile and os.path.isfile(outfile):
-        os.unlink(outfile)
-
-    grammar = codecs.open(filename, 'r', encoding='utf-8').read()
-
+    outfile = args.outfile
     if outfile:
         dirname = os.path.dirname(outfile)
         if dirname and not os.path.isdir(dirname):
             os.makedirs(dirname)
 
+    grammar = codecs.open(args.filename, 'r', encoding='utf-8').read()
+
     try:
         model = genmodel(
-            name,
+            args.name,
             grammar,
-            trace=trace,
-            filename=filename,
-            colorize=colorize
+            trace=args.trace,
+            filename=args.filename,
+            colorize=args.color
         )
-        model.whitespace = whitespace
-        model.nameguard = False if not nameguard else None  # None allows grammar specified or the default of True
-        model.left_recursion = left_recursion
+        model.whitespace = args.whitespace
+        model.nameguard = args.nameguard
+        model.left_recursion = args.left_recursion
 
-        if pretty:
-            result = str(model)
-        elif object_model:
-            result = objectmodel.codegen(model)
-        else:
-            result = codegen(model)
-
-        if draw:
+        if args.draw:
             from grako import diagrams
             diagrams.draw(outfile, model)
-        elif outfile:
-            with codecs.open(outfile, 'w', encoding='utf-8') as f:
-                f.write(result)
         else:
-            print(result)
+            if args.pretty:
+                result = str(model)
+            elif args.object_model:
+                result = objectmodel.codegen(model)
+            else:
+                result = codegen(model)
+
+            if outfile:
+                with codecs.open(outfile, 'w', encoding='utf-8') as f:
+                    f.write(result)
+            else:
+                print(result)
 
         print('-' * 72, file=sys.stderr)
         print('{:12,d}  lines in grammar'.format(len(grammar.split())), file=sys.stderr)
         print('{:12,d}  rules in grammar'.format(len(model.rules)), file=sys.stderr)
         print('{:12,d}  nodes in AST'.format(model.nodecount()), file=sys.stderr)
     except GrakoException as e:
-        _error(e)
+        print(e, file=sys.stderr)
         sys.exit(1)
 
 if __name__ == '__main__':
