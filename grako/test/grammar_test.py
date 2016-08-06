33bd2f90e85d7986978233afb33de7926f8e9f3a
apalala <apalala@gmail.com>
2016-08-06 06:55:42 -0400

    Positive and normal joins implemented.
    --HG--
    branch : normal_joins

--- grako/test/grammar_test.py
+++ grako/test/grammar_test.py
@@ -834,17 +834,17 @@ class GrammarTests(unittest.TestCase):
         ast = model.parse("xxxy", nameguard=False)
         self.assertEqual([['x', 'x', 'x'], [], 'y'], ast)
 
-    def test_join(self):
+    def test_positive_join(self):
         grammar = '''
-            start = ','.{'x' 'y'} ;
+            start = ','.{'x' 'y'}+ ;
         '''
 
         grammar2 = '''
-            start = (','.{'x'}|{}) ;
+            start = (','.{'x'}+|{}) ;
         '''
 
         grammar3 = '''
-            start = [','.{'x'}] ;
+            start = [','.{'x'}+] ;
         '''
 
         model = genmodel("test", grammar)
@@ -871,9 +871,26 @@ class GrammarTests(unittest.TestCase):
         ast = model.parse("y x", nameguard=False)
         self.assertEqual(None, ast)
 
+    def test_normal_join(self):
+        grammar = '''
+            start = ','.{'x' 'y'} 'z' ;
+        '''
+
+        model = genmodel("test", grammar)
+        codegen(model)
+
+        ast = model.parse("x y, x y z", nameguard=False)
+        self.assertEqual([[['x', 'y'], ['x', 'y']], 'z'], ast)
+
+        ast = model.parse("x y z", nameguard=False)
+        self.assertEqual([[['x', 'y']], 'z'], ast)
+
+        ast = model.parse("z", nameguard=False)
+        self.assertEqual([[], 'z'], ast)
+
     def test_group_join(self):
         grammar = '''
-            start = ('a' 'b').{'x'} ;
+            start = ('a' 'b').{'x'}+ ;
         '''
         model = genmodel("test", grammar)
         c = codegen(model)
@@ -890,7 +907,7 @@ class GrammarTests(unittest.TestCase):
             @@keyword :: B C
             @@keyword :: 'A'
 
-            start = ('a' 'b').{'x'} ;
+            start = ('a' 'b').{'x'}+ ;
         '''
         model = genmodel("test", grammar)
         c = codegen(model)
