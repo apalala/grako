bfb070367c33d5519633df0d9e15f8b722e61d87
apalala <apalala@gmail.com>
2016-09-23 11:07:40 -0400

    also reexport AST from grako.model

--- grako/model.py
+++ grako/model.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from grako.ast import AST                           # noqa: F401
 from grako.objectmodel import Node                  # noqa: F401
 from grako.objectmodel import Node as ParseModel    # noqa: F401
 from grako.walkers import NodeWalker                # noqa: F401
