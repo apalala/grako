3e3bd6aad8e508a03ab7b72a597d973391f057a8
apalala <apalala@gmail.com>
2017-02-14 16:44:57 -0400

    flake8

--- grako/_version.py
+++ grako/_version.py
@@ -2,4 +2,4 @@
 # Copyright (C) 2017      by Juancarlo Añez
 # Copyright (C) 2012-2016 by Juancarlo Añez and Thomas Bragg
 
-from grako._config import __version__
+from grako._config import __version__  # noqa
